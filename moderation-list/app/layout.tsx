48b445246b930a08bdee78b030912c9b16ed29d9
Isaac Adedokun <isaacadedokun111@gmail.com>
2026-01-07 16:13:40 +0100

    newChangesMade

--- moderation-list/app/layout.tsx
+++ moderation-list/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
 import Navbar from "./navbar";
-
+// import IssuesPage from "./issues/page";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
--- moderation-list/app/navbar.tsx
+++ moderation-list/app/navbar.tsx
@@ -1,15 +1,36 @@
-import React from "react";
-import Link from "next/link";
+"use client";
 
+import Link from "next/link";
+import { AiFillBug } from "react-icons/ai";
+import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const Navbar = () => {
+  const CurrentPath = usePathname();
+  console.log(CurrentPath);
+  const Links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
   return (
-    <nav className="flex space-x-6">
-      <Link href="/">Logo</Link>
-      <ul className="flex">
-        <li><Link href="/">Dashboard</Link></li>
-        <li><Link href="/issues">Issues</Link></li>
-        <li></li>
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex  space-x-6">
+        {Links.map((Links) => (
+          <Link
+            key={Links.href}
+            className={classnames({
+              "text-zinc-900": Links.href === CurrentPath,
+              "text-zinc-500": Links.href !== CurrentPath,
+              "hover:text-zinc-800 transition-colors": true,
+            })}
+            href={Links.href}
+          >
+            {Links.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
